72566e95e048ca85f38191d43329db636afcc890
markus_hu <15055667366@qq.com>
2021-07-15 13:58:05 +0800

    upd: upd postinstall。

--- src/postInstall.js
+++ src/postInstall.js
@@ -2,7 +2,7 @@ const path = require('path')
 const fs = require('fs')
 function postInstallFunc () {
   fs.readFile(path.resolve('../../.gitignore'), "utf8" , (err, data) => {
-    if(err) throw err
+    if (err) { }
     if (!data.includes("/swagger_interface")) {
       fs.appendFileSync(path.resolve('../../.gitignore'),"\n/swagger_interface", "utf8")
       console.log('updated gitignore')
